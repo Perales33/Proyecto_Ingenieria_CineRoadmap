bd16cdef1b6b1816348b2c1910e3f3de6537d892
Perales33 <perales.antonio33@hotmail.com>
2025-12-03 01:18:35 +0100

    Añadir campo fecha a la calificación

--- src/main/app/Modelo/Calificacion.java
+++ src/main/app/Modelo/Calificacion.java
@@ -1,12 +1,14 @@
 package main.app.Modelo;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Calificacion
 {
     private int calificacion;
     private Pelicula pelicula;
     private Usuario usuario;
+    private Date fechaCalificacion;
     private static ArrayList<Calificacion> listaCalificaciones = new ArrayList<>();
 
     public Calificacion(int calif, Pelicula pel, Usuario us)
@@ -14,12 +16,14 @@ public class Calificacion
         calificacion = calif;
         pelicula = pel;
         usuario = us;
+        fechaCalificacion = new Date();
     }
 
     public int getCalificacion() { return calificacion; }
     public void setCalificacion(int calificacion) { this.calificacion = calificacion; }
     public Pelicula getPelicula() { return pelicula; }
     public Usuario getUsuario() { return usuario; }
+    public Date getFechaCalificacion() { return fechaCalificacion; }
 
     public static ArrayList<Calificacion> getListaCalificaciones() { return listaCalificaciones; }
 }
