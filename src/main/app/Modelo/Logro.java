7b495b1963be7f549f36bc9274abe5392b7e4eed
sanlopmat <sanlopmat06@gmail.com>
2026-01-25 12:10:00 +0100

    Update Logro.java

--- src/main/app/Modelo/Logro.java
+++ src/main/app/Modelo/Logro.java
@@ -1,1 +1,35 @@
+public class Logro {
 
+    private String nombreLogro;
+    private String descripcion;
+    private boolean completo;
+    private Insignia insignia;
+    private Reto reto;
+    private ProgresoLogrosInsignias progreso;
+
+    public Logro(String nombre, String descripcion, Insignia ins, Reto reto) {
+        this.nombreLogro = nombre;
+        this.descripcion = descripcion;
+        this.insignia = ins;
+        this.reto = reto;
+        this.progreso = new ProgresoLogrosInsignias(0, reto.getProgresoObjetivo());
+    }
+
+    public void syncConReto() {
+        progreso.setActual(reto.getProgresoActual());
+        progreso.setObjetivo(reto.getProgresoObjetivo());
+
+        if (reto.getEstado() == Reto.Estado.COMPLETADO) {
+            completo = true;
+            insignia.desbloquear();
+        }
+    }
+
+    public int getProgresoActual() { return progreso.getActual(); }
+    public int getProgresoObjetivo() { return progreso.getObjetivo(); }
+    public boolean getCompleto() { return completo; }
+
+    public Insignia getInsignia() { return insignia; }
+    public String getDescripcion() { return descripcion; }
+    public String getnombreReto() { return nombreLogro; }
+}
